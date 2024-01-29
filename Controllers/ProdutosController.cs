303825ab089e2874085e548c5b99009d351a8fc3
João Pedro Fernandes Marques <joao.fernandes@linearsistemas.com.br>
2024-01-28 21:13:57 -0300

    primeiro commit

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,71 @@
+﻿using ApiProduto.Data;
+using ApiProduto.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProduto.Controllers
+{
+    [Route("api/produtos")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+        //public ProdutosController(ApiDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
+        public ActionResult GetProdutos()
+        {
+            return Ok(new Produto { Id = 1, Nome = "Teste", QuantidadeEstoque = 10 });
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult Get(int id)
+        {
+            return Ok(new Produto { Id = 1, Nome = "Teste" });
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post(Produto produto)
+        {
+            if (produto == null)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction("Get", new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiProduto.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProduto.Data
+{
+    public class ApiDbContext : IdentityDbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
